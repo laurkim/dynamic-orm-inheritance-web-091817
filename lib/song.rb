a86e8b3f4d08813cc94d2f72fdf7f478d7067fc7
laurkim <laurakeem@gmail.com>
2017-09-26 15:58:56 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,11 @@
 require_relative "./interactive_record.rb"
+require 'pry'
 
 class Song < InteractiveRecord
 
   self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
   end
+
 
 end
